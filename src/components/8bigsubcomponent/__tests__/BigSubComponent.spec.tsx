f2c34c7f5d0acc04a4fa890419604b4b7817903b
Mati20041 <mat20041+github@gmail.com>
2022-10-06 15:32:39 +0200

    added test to BigSubComponent

--- src/components/8bigsubcomponent/__tests__/BigSubComponent.spec.tsx
+++ src/components/8bigsubcomponent/__tests__/BigSubComponent.spec.tsx
@@ -1,8 +1,25 @@
 import {BigSubComponent} from "../BigSubComponent";
+import {render, screen} from "@testing-library/react";
 
+jest.mock("../SubComponent", () => ({
+    SubComponent: (props: { projectId: number }) => <div data-testid="sub-component">{props.projectId}</div>
+}))
 
-describe('BigSubComponent', function () {
-    it('should be ok', function () {
-        expect(2).toEqual(2)
+describe('BigSubComponent', () => {
+    it('should be ok', () => {
+        // Arrange & Act
+        const {getSubComponent} = renderBigSubComponent()
+        const subComponent = getSubComponent();
+
+        // Assert
+        expect(subComponent).toHaveTextContent('5')
     });
 });
+
+const renderBigSubComponent = () => {
+    render(<BigSubComponent/>)
+
+    return {
+        getSubComponent: () => screen.getByTestId('sub-component')
+    }
+}
